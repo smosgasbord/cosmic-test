c99c12afbd652c567c3b5c1630c10f88885ad32b
smosgasbord <saenz.ran@gmail.com>
2022-05-30 01:38:01 -0600

    Create BlogFooter.vue

---
+++ src/components/BlogFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <transition appear name="v--mask">
+    <footer v-once class="blog__footer">
+      <div class="signature cosmicjs">
+       
+      </div>
+    </footer>
+  </transition>
+</template>
+
+<script>
+export default { name: 'blog-footer' }
+</script>
